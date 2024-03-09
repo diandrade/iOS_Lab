5047a2c617be6b2f05979c1083e18a78fb5fd74a
Diego Andrade <81432715+diandrade@users.noreply.github.com>
2024-03-09 13:55:29 -0300

    Add files via upload

---
+++ Development/I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by IOS Lab on 09/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var sum: UIButton!
+    @IBOutlet weak var sub: UIButton!
+    @IBOutlet weak var div: UIButton!
+    @IBOutlet weak var multi: UIButton!
+    @IBOutlet weak var input2: UITextField!
+    @IBOutlet weak var input1: UITextField!
+    @IBOutlet weak var result: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

b9b6e83aa3a0f622aa7ada1d4dadec87c6275b5d
iOSLab <iOSLab@USJT-iOSLab-04.local>
2024-04-13 16:00:16 -0300

    Adição de novas funcionalidades no Fruits

--- Development/Fruits/Fruits/ViewController.swift
+++ Development/Fruits/Fruits/ViewController.swift
@@ -8,15 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var fruits: [String] = []
+    var fruits: [String: Bool] = [:]
     
     @IBOutlet weak var inputFruit: UITextField!
+    @IBOutlet weak var withLamp: UISwitch!
+    @IBOutlet weak var withoutLamp: UISwitch!
+    @IBOutlet weak var searchFruit: UITextField!
+    @IBOutlet weak var lampControl: UISegmentedControl!
     @IBOutlet weak var outputFruit: UITextView!
     
-    private func renderUI() {
+    private func checkSeed(title: String) -> Bool {
+        title.contains("Com")
+    }
+    
+    private func renderUI(fruits collection: [String: Bool]? = nil) {
+        let collectionFruits: [String: Bool] = collection ?? fruits
         var outputText = ""
-        for (index, fruit) in fruits.enumerated() {
-            outputText = outputText + "Fruta \(index): \(fruit) \n"
+        for (index, fruit) in collectionFruits.enumerated() {
+            outputText = outputText + "Fruta \(index): \(fruit.key) \n"
         }
         outputFruit.text = outputText
     }
@@ -30,21 +39,63 @@ class ViewController: UIViewController {
     @IBAction func add(_ sender: Any) {
         guard let fruit = inputFruit.text,
               !fruit.isEmpty,
-              !fruits.contains(fruit) else {
+              !fruits.keys.contains(fruit) else {
             renderError()
             return
         }
-        fruits.append(fruit)
+        fruits.updateValue(checkSeed(title: lampControl.titleForSegment(at: lampControl.selectedSegmentIndex) ?? ""), forKey: fruit.lowercased())
         renderUI()
     }
     
     @IBAction func remove(_ sender: Any) {
-        guard let fruit = inputFruit.text, 
-              let index = fruits.firstIndex(of: fruit) else {
+        guard let fruit = inputFruit.text,
+                let index = fruits.index(forKey: fruit.lowercased()) else {
             renderError()
             return
         }
         fruits.remove(at: index)
         renderUI()
     }
+    
+    @IBAction func search(_ sender: Any) {
+        guard let searchFruit = searchFruit.text else {
+            renderError()
+            return
+        }
+        
+        var searchFruits: [String: Bool] = [:]
+        
+        if withLamp.isOn {
+            let filteredFruits = fruits.filter { (key: String, value: Bool) in
+                value == true
+            }
+            
+            searchFruits.merge(filteredFruits) { _, _ in
+                false
+            }
+        }
+        
+        if withoutLamp.isOn {
+            let filteredFruits = fruits.filter { (key: String, value: Bool) in
+                value == false
+            }
+            
+            searchFruits.merge(filteredFruits) { _, _ in
+                false
+            }
+        }
+        
+        if !searchFruit.isEmpty {
+            let filteredFruits = searchFruits.filter { (key: String, value: Bool) in
+                key.contains(searchFruit.lowercased())
+            }
+            
+            searchFruits = filteredFruits
+        }
+        
+        renderUI(fruits: searchFruits)
+    }
+    
 }
+    
+
